ed1d003eecaeb29c544f90ecc58051d6b6099414
Deise santana Pereira <deise0509@gmail.com>
2023-01-06 20:14:45 -0300

    configuração do pom e properties ok

---
+++ trabalhando-com-queue/src/main/java/br/com/trabalhandocomqueue/TrabalhandoComQueueApplication.java
@@ -0,0 +1,13 @@
+package br.com.trabalhandocomqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhandoComQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhandoComQueueApplication.class, args);
+	}
+
+}
---
+++ trabalhando-com-queue/src/test/java/br/com/trabalhandocomqueue/TrabalhandoComQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.trabalhandocomqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhandoComQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
